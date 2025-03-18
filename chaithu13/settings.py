535714b65a43cd074e35f0931d545bed2bf20f22
Chaithu1342 <136830895+Chaithu1342@users.noreply.github.com>
2025-03-18 20:24:21 +0530

    Removed SQLite and configured PostgreSQL for Render

--- chaithu13/settings.py
+++ chaithu13/settings.py
@@ -103,12 +103,13 @@ WSGI_APPLICATION = 'chaithu13.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 # Password validation
