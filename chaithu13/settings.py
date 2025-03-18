f5db2211930146685e0fefe453b5cfecebcf2e63
Chaithu1342 <136830895+Chaithu1342@users.noreply.github.com>
2025-03-18 13:44:48 +0530

    Your commit message here

--- chaithu13/settings.py
+++ chaithu13/settings.py
@@ -16,7 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR= "templates"/BASE_DIR
-STATICFILES_DIRS="static"/BASE_DIR
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 # Quick-start development settings - unsuitable for production
